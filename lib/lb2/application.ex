e70d208da064508c62139528ca36c956ca942aaf
thread <thethread@gmail.com>
2018-11-21 17:09:41 -0500

    banner

--- lib/lb2/application.ex
+++ lib/lb2/application.ex
@@ -5,6 +5,8 @@ defmodule Lb2.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
+    IO.puts(banner())
+
     children = [
       supervisor(Lb2.Repo, []),
       supervisor(Lb2Web.Endpoint, [])
@@ -18,4 +20,13 @@ defmodule Lb2.Application do
     Lb2Web.Endpoint.config_change(changed, removed)
     :ok
   end
+
+  def banner,
+    do: ~S"""
+         __            _     _ _                         _
+        / / _   _  ___(_) __| | |__   ___   __ _ _ __ __| |
+       / / | | | |/ __| |/ _` | '_ \ / _ \ / _` | '__/ _` |
+      / /__| |_| | (__| | (_| | |_) | (_) | (_| | | | (_| |
+      \____/\__,_|\___|_|\__,_|_.__/ \___/ \__,_|_|  \__,_|
+    """
 end
