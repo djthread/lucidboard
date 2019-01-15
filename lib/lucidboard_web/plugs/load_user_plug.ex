7a20082df8d4c677f8c8ea4a1b51a8a5a18eee62
Adam Bellinson <thread@threadbox.net>
2019-01-15 16:22:42 -0500

    test fixes

--- lib/lucidboard_web/plugs/load_user_plug.ex
+++ lib/lucidboard_web/plugs/load_user_plug.ex
@@ -9,7 +9,7 @@ defmodule LucidboardWeb.LoadUserPlug do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    user = Repo.one(from(User, limit: 1))
+    user = get_a_user_i_dont_care()
     IO.puts "Loaded user with theme #{user.settings.theme}"
 
     theme_css =
@@ -20,5 +20,10 @@ defmodule LucidboardWeb.LoadUserPlug do
     conn
     |> assign(:user, user)
     |> assign(:theme_css, theme_css <> ".css")
+  end
+
+  defp get_a_user_i_dont_care do
+    Repo.one(from(u in User, where: u.name == "bob"))
+    || Repo.insert!(User.new(name: "bob"))
   end
 end
