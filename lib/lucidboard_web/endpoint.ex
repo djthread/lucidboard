81c54cd92d916c5eddb485c1026db06914fd8cd8
Adam Bellinson <thread@threadbox.net>
2019-03-19 09:06:30 -0400

    little cleanup

--- lib/lucidboard_web/endpoint.ex
+++ lib/lucidboard_web/endpoint.ex
@@ -3,18 +3,9 @@ defmodule LucidboardWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket
 
-  # socket "/socket", LucidboardWeb.UserSocket,
-  #   websocket: true,
-  #   longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
     from: :lucidboard,
-    gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
