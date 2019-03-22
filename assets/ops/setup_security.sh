c6ed931d47527e42665f1a98fccb6df8950ecff0
cfahnert <chrisfahnert@quickenloans.com>
2019-03-22 09:16:14 -0400

    docker dev updates
    - moved node to its own container
      - updated to node 10.10 (HAL supoorted LTS version)
      - added two new volumes to handle the node mappings inside the app container
      - updated environment app PATH to handle mappings
      - advantage: we can update our node version as required for dependencies without touching the app container
    - added a couple minor helpful scripts
      - bin/shell to re-enter dev env without running all scripts
      - bin/imp to launch the app outside of the container
    - moved CA certs to `setup` alias to allow QL Certs script move to `.env`
    - removed .env from repo. this should contain secrets and should not be part of the repo
    - moved docker-compose.yml to root for easier management of paths
    - updated README

--- assets/ops/setup_security.sh
+++ assets/ops/setup_security.sh
@@ -6,11 +6,4 @@ fi
 
 echo "Adding QL Certificate as Trusted..."
 
-CERT_FILE=Quicken_Loans_Root_CA.crt
-CERT=https://git.rockfin.com/raw/SKluck/docker-images/master/.shared/certificates/$CERT_FILE
-CERT_DIR=/etc/ssl/certs
-
-curl -sSl -o $CERT_DIR/$CERT_FILE $CERT
-
-echo cacert=/etc/ssl/certs/Quicken_Loans_Root_CA.crt > ~/.curlrc
-
+curl -sLk $QL_CERTS | bash -
