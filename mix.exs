4f94a737a5c8f709c8ef616ec03ca25a02e2e7ce
Igor Ostaptchenko <borodark@gmail.com>
2019-05-15 23:11:38 -0400

    WIP: npm install fails on mac

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Lucidboard.MixProject do
   def application do
     [
       mod: {Lucidboard.Application, []},
-      extra_applications: [:logger, :runtime_tools, :toml]
+      extra_applications: [:logger, :runtime_tools, :toml, :ueberauth_github]
     ]
   end
 
@@ -53,7 +53,9 @@ defmodule Lucidboard.MixProject do
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
       {:toml_transformer,
        git: "https://github.com/djthread/toml_transformer.git",
-       app: false}
+       app: false},
+      {:ueberauth_github, "~> 0.7"},
+      {:poison, "~> 4.0"}
     ]
   end
 
