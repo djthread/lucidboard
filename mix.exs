570f348a088dbe1cf6d02af44f8e68c405869585
Thread <thread@threadbox.net>
2019-05-27 18:02:09 -0400

    fixes for deployment

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Lucidboard.MixProject do
   def application do
     [
       mod: {Lucidboard.Application, []},
-      extra_applications: [:logger, :runtime_tools, :toml]
+      extra_applications: [:logger, :runtime_tools, :toml, :toml_transformer]
     ]
   end
 
