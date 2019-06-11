7acaa2cee2d328025e5452323c68995fe454a31a
Adam Bellinson <thread@threadbox.net>
2019-06-11 14:13:37 -0400

    body_class()

--- lib/lucidboard_web/views/layout_view.ex
+++ lib/lucidboard_web/views/layout_view.ex
@@ -5,4 +5,7 @@ defmodule LucidboardWeb.LayoutView do
   def render_layout(outer_layout, assigns, do: content) do
     render(outer_layout, Map.put(assigns, :inner_layout, content))
   end
+
+  defp body_class(nil), do: "t-light"
+  defp body_class(%{settings: %{theme: theme}}), do: "t-#{theme}"
 end
