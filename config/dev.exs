9f273a7c6afea662649294bfb8787a1c6eb2a30d
Adam Bellinson <thread@threadbox.net>
2019-06-12 16:32:36 -0400

    ok

--- config/dev.exs
+++ config/dev.exs
@@ -10,16 +10,16 @@ config :lucidboard, LucidboardWeb.Endpoint,
   http: [port: 8800],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false
-  # watchers: [
-  #   node: [
-  #     "node_modules/webpack/bin/webpack.js",
-  #     "--mode",
-  #     "development",
-  #     "--watch-stdin",
-  #     cd: Path.expand("../assets", __DIR__)
-  #   ]
-  # ]
+  check_origin: false,
+  watchers: [
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # ## SSL Support
 #
