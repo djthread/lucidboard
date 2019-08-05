908941f2018cff353a8bfb78744fade41dfe2447
Adam Bellinson <thread@threadbox.net>
2019-08-05 15:34:57 -0400

    pingfed fix

--- config/config.exs
+++ config/config.exs
@@ -57,7 +57,7 @@ config :oauth2, serializers: %{"application/json" => Jason}
 config :ueberauth, Ueberauth,
   providers: [
     github: {Ueberauth.Strategy.Github, default_scope: "user:email"},
-    pingfed: {Ueberauth.Strategy.PingFed, default_scope: "openid profile email"}
+    pingfed: {Ueberauth.Strategy.PingFed, default_scope: "openid profile"}
   ]
 
 # config :ueberauth, Ueberauth.Strategy.Github.OAuth
