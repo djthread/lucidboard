390eb4ef7462cb6399ce2fd590acd8aae039feb0
Adam Bellinson <thread@threadbox.net>
2019-08-14 09:28:07 -0400

    Changelog (#66)
    * changelog page
    
    * .
    
    * footer refinement
    
    * .
    
    * read only board options view
    
    * role manager javascript fix
    
    * .
    
    * making some stuff look decent (#65)
    
    * changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## 2019-08-09
+
+* Started `CHANGELOG.md` & added it to the site at `/changelog`
+* Added a footer with useful links
+* Created "show" board options screen for non-owners
+* Fixed likes per user per card validation
+* Switched to pure Elixir 1.9-based releases for deployment
--- lib/lucidboard_web/views/page_view.ex
+++ lib/lucidboard_web/views/page_view.ex
@@ -1,3 +1,16 @@
 defmodule LucidboardWeb.PageView do
   use LucidboardWeb, :view
+
+  @changelog_html [File.cwd!(), "CHANGELOG.md"]
+                  |> Path.join()
+                  |> File.read!()
+                  |> Earmark.as_html!()
+
+  def render("changelog.html", _params) do
+    raw("""
+    <div class="content">
+      #{@changelog_html}
+    </div>
+    """)
+  end
 end
