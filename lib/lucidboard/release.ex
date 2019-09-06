51f6b195134c8fa64597b664e699178edc83183b
Adam Bellinson <thread@threadbox.net>
2019-09-06 10:03:28 -0400

    run migrations on (released) app startup

---
+++ lib/lucidboard/release.ex
@@ -0,0 +1,29 @@
+defmodule Lucidboard.Release do
+  @moduledoc "Do tasks related to deployment"
+  alias Ecto.Migrator
+
+  @app :lucidboard
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+
+    {:ok, _, _} =
+      Migrator.with_repo(repo, &Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.ensure_all_started(@app)
+  end
+end
